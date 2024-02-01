5506c1a2488c750bc410012c6ef351b70ecf3420
xasan <xasan>
2024-02-01 17:48:56 +0500

    heade changes

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Logo from "../../assets/img/logo.svg";
-import { links } from "./data";
-import { HeaderButtons } from "./components/header-buttons/header-buttons";
-import { Link } from "react-router-dom";
+// import { links } from "./data";
+// import { HeaderButtons } from "./components/header-buttons/header-buttons";
+// import { Link } from "react-router-dom";
+
 import "./header.scss"
 
 export const Header = () => {
@@ -10,14 +11,20 @@ export const Header = () => {
     <header className="header">
       <div className="container box">
         <img src={Logo} alt="" />
-        <ul>
+        <nav className="header__nav">
+          <div className="header__tel">
+            <Tel/>
+          </div>
+        </nav>
+
+        {/* <ul>
           {links.map((item) => (
             <Link key={item.path} to={item.path}>
               {item.name}
             </Link>
           ))}
-        </ul>
-        <HeaderButtons />
+        </ul> */}
+        {/* <HeaderButtons /> */}
       </div>
     </header>
   );
